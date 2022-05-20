a679eed84f1950567dfd84ea5297fd877e0d7dad
GamerBike39 <champidub@gmail.com>
2022-05-20 09:45:50 +0100

    correctif

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,20 @@
+const burger = document.querySelector(".burger");
+const burgerMenu = document.querySelector(".burgerMenu");
+const easterEggs = document.querySelector(".title2");
+const scroll = (window.location.hash = "awesome");
 
+function toggleMenu() {
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("showMenu");
+  });
+}
+toggleMenu();
+
+// EasterEggs title
+
+function ahah() {
+  easterEggs.addEventListener("click", () => {
+    easterEggs.classList.toggle("animate__hinge");
+  });
+}
+ahah();
